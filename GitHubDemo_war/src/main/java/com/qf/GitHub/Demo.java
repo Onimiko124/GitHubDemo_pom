eb90ebe8f225fbcde9e16ceb13f79b43ffd6c628
Onimiko124 <43039413+Onimiko124@users.noreply.github.com>
2018-09-12 19:32:14 +0800

    Update Demo.java
    添加了一行注释

--- GitHubDemo_war/src/main/java/com/qf/GitHub/Demo.java
+++ GitHubDemo_war/src/main/java/com/qf/GitHub/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main(String[] args){
         /**
          *  普通的for循环
+         *  添加一行注释
          */
         for(int i=0;i<10;i++){
             System.out.println(i++);
