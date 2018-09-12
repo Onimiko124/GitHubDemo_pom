2740bbcbc8898169d6f011494118627e0aa274d5
Onimico124 <1306528188@qq.com>
2018-09-12 19:26:53 +0800

    Initial commit

---
+++ GitHubDemo_war/src/main/java/com/qf/GitHub/Demo.java
@@ -0,0 +1,12 @@
+package com.qf.GitHub;
+
+public class Demo {
+    public static void main(String[] args){
+        /**
+         *  普通的for循环
+         */
+        for(int i=0;i<10;i++){
+            System.out.println(i++);
+        }
+    }
+}
